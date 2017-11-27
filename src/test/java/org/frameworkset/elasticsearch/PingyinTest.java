1a3af456681add89d30887f279a240e194826b8d
yinbp <yin-bp@163.com>
2017-11-28 00:15:25 +0800

    拼音检索测试用例添加
    拼音检索测试用例添加

--- src/test/java/org/frameworkset/elasticsearch/PingyinTest.java
+++ src/test/java/org/frameworkset/elasticsearch/PingyinTest.java
@@ -1,9 +1,12 @@
 package org.frameworkset.elasticsearch;
 
+import com.frameworkset.util.FileUtil;
 import org.frameworkset.elasticsearch.client.ClientInterface;
+import org.frameworkset.elasticsearch.client.ClientUtil;
 import org.frameworkset.elasticsearch.entity.Demo;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -80,5 +83,132 @@ public class PingyinTest {
 				"demo",//索引类型
 				"3",//创建文档对应的脚本名称，在esmapper/estrace/ESTracesMapper.xml中配置
 				Demo.class);
+	}
+
+
+	@Test
+	public void testBulkAddDateDocument1() throws ParseException {
+//		testCreateDemoMapping();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+		String date = format.format(new Date());
+		ClientInterface clientUtil = ElasticSearchHelper.getRestClientUtil();
+		List<Demo> demos = new ArrayList<>();
+		Demo demo = new Demo();
+		demo.setDemoId(12l);
+		demo.setAgentStarttime(new Date());
+		demo.setApplicationName("blackcatdemo2");
+		demo.setContentbody("this is content body2");
+		demo.setName("liu德华");
+		demos.add(demo);
+
+		demo = new Demo();
+		demo.setDemoId(13l);
+		demo.setAgentStarttime(new Date());
+		demo.setApplicationName("blackcatdemo3");
+		demo.setContentbody("四大天王，这种文化很好，中华人民共和国");
+		demo.setName("zhang学友");
+		demos.add(demo);
+
+		//创建模板
+		String response = clientUtil.addDateDocuments("demo",//索引表
+				"demo",//索引类型
+				demos);
+
+		System.out.println("addDateDocument-------------------------");
+		System.out.println(response);
+
+		response = clientUtil.getDocument("demo-"+date,//索引表
+				"demo",//索引类型
+				"12");
+		System.out.println("getDocument-------------------------");
+		System.out.println(response);
+
+		demo = clientUtil.getDocument("demo-"+date,//索引表
+				"demo",//索引类型
+				"13",//创建文档对应的脚本名称，在esmapper/estrace/ESTracesMapper.xml中配置
+				Demo.class);
+	}
+
+	@Test
+	public void testPboosMapAdressTemplate(){
+		ClientInterface clientUtil = ElasticSearchHelper.getConfigRestClientUtil("esmapper/estrace/pinyin.xml");
+		try {
+
+			String template = clientUtil.deleteTempate("pboosmapadresstemplate");
+			System.out.println(template);
+
+		} catch (ElasticSearchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			String template = clientUtil.createTempate("pboosmapadresstemplate", "pboosMapAdressPinyinTemplate");
+			System.out.println(template);
+		} catch (ElasticSearchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+
+			clientUtil.dropIndice("pboos-map-adress-1503973107");
+//			//获取索引表结构
+//			System.out.println(clientUtil.getIndice("demo"));
+//			//删除索引表结构
+//			System.out.println(clientUtil.dropIndice("demo"));
+		} catch (ElasticSearchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void importPboosMapData() throws IOException {
+		String data = FileUtil.getFileContent("F:\\4_ASIA文档\\1_项目\\13_江西移动\\拼音搜索\\py\\maps_0","UTF-8");
+//		StringReader reader = new StringReader(data);
+//		LineNumberReader lr = new LineNumberReader(reader);
+//		String line = null;
+//		StringBuilder builder = new StringBuilder();
+//		int i = 0;
+//		do{
+//			line = lr.readLine();
+//
+//			System.out.println(line);
+//
+//			if( line == null)
+//				break;
+//			else {
+//				builder.append(line).append("\n");
+//
+//				i ++;
+//				if(i == 5000)
+//					break;
+//
+//			}
+//		}while (true);
+
+
+		//System.out.print(data);
+		ClientInterface clientUtil = ElasticSearchHelper.getRestClientUtil();
+		String temp = clientUtil.executeHttp("_bulk",data, ClientUtil.HTTP_POST);
+//		System.out.println(temp);
+	}
+
+	@Test
+	public void updateDocument(){
+		ClientInterface clientUtil = ElasticSearchHelper.getConfigRestClientUtil("esmapper/estrace/pinyin.xml");
+		String temp = clientUtil.executeHttp("pboos-map-adress-1503973107/boosmap/38130122/_update",
+				"updateDocument",
+				(Object)null,
+				ClientInterface.HTTP_POST);
+		System.out.println(temp);
+
+	}
+
+
+	@Test
+	public void searchPinyin(){
+
 	}
 }
--- src/test/java/org/frameworkset/elasticsearch/SuggestionTest.java
+++ src/test/java/org/frameworkset/elasticsearch/SuggestionTest.java
@@ -61,16 +61,109 @@ public class SuggestionTest {
 				Demo.class);
 	}
 	@Test
-	public void testSuggestion(){
+	public void testIndex1(){
+		ClientInterface clientUtil = ElasticSearchHelper.getRestClientUtil();
+		List<Demo> demos = new ArrayList<>();
+		Demo demo = new Demo();
+		demo.setDemoId(7l);
+		demo.setAgentStarttime(new Date());
+		demo.setApplicationName("blackcatdemo2");
+		demo.setContentbody("this is content body2");
+		demo.setName("四大天王，这种刘德华文化很好，张学友中华人民共和国刘德华");
+		demos.add(demo);
+
+		demo = new Demo();
+		demo.setDemoId(8l);
+		demo.setAgentStarttime(new Date());
+		demo.setApplicationName("blackcatdemo3");
+		demo.setContentbody("四大天王，这种文化很好，中华人民共和国");
+		demo.setName("古巨基刘德华张学友");
+		demos.add(demo);
+
+		demo = new Demo();
+		demo.setDemoId(9l);
+		demo.setAgentStarttime(new Date());
+		demo.setApplicationName("blackcatdemo3");
+		demo.setContentbody("四大天王，这种文化很好，中华人民共和国");
+		demo.setName("刘德华");
+		demos.add(demo);
+
+		demo = new Demo();
+		demo.setDemoId(10l);
+		demo.setAgentStarttime(new Date());
+		demo.setApplicationName("blackcatdemo3");
+		demo.setContentbody("四大天王，这种文化很好，中华人民共和国");
+		demo.setName("刘德一");
+		demos.add(demo);
+
+		//创建模板
+		String response = clientUtil.addDocuments("suggestion",//索引表
+				"suggest",//索引类型
+				demos);
+
+		System.out.println("addDateDocument-------------------------");
+		System.out.println(response);
+
+		response = clientUtil.getDocument("suggestion",//索引表
+				"suggest",//索引类型
+				"10");
+		System.out.println("getDocument-------------------------");
+		System.out.println(response);
+
+		demo = clientUtil.getDocument("suggestion",//索引表
+				"suggest",//索引类型
+				"9",//创建文档对应的脚本名称，在esmapper/estrace/ESTracesMapper.xml中配置
+				Demo.class);
+	}
+	@Test
+	public void testComplete(){
 		ClientInterface clientUtil = ElasticSearchHelper.getConfigRestClientUtil("esmapper/estrace/suggest.xml");
 		try {
 			Map<String,Object> params = new HashMap<String,Object>();
-			params.put("prefix","liu de hua zhangxueyou");
+			params.put("prefix","liu");
 			//clientUtil.dropIndice("suggestion");
 			String template = clientUtil.executeHttp("suggestion/suggest/_search?pretty",
-														"doSuggest",
+														"doComplete",
 														params,
 														ClientInterface.HTTP_POST);
+			System.out.println(template);
+
+		} catch (ElasticSearchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testTerm(){
+		ClientInterface clientUtil = ElasticSearchHelper.getConfigRestClientUtil("esmapper/estrace/suggest.xml");
+		try {
+			Map<String,Object> params = new HashMap<String,Object>();
+			params.put("prefix","this is content");
+			//clientUtil.dropIndice("suggestion");
+			String template = clientUtil.executeHttp("suggestion/suggest/_search?pretty",
+					"doTerm",
+					params,
+					ClientInterface.HTTP_POST);
+			System.out.println(template);
+
+		} catch (ElasticSearchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testPhrase(){
+		ClientInterface clientUtil = ElasticSearchHelper.getConfigRestClientUtil("esmapper/estrace/suggest.xml");
+		try {
+			Map<String,Object> params = new HashMap<String,Object>();
+			params.put("prefix","this is content");
+			//clientUtil.dropIndice("suggestion");
+			String template = clientUtil.executeHttp("suggestion/suggest/_search?pretty",
+					"doPhrase",
+					params,
+					ClientInterface.HTTP_POST);
 			System.out.println(template);
 
 		} catch (ElasticSearchException e) {
