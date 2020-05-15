ebac26e0a9057ac81167855f08f030ebf374bd51
yinbp <yin-bp@163.com>
2020-05-15 20:54:19 +0800

    简化httpclient组件处理逻辑

--- src/main/java/org/frameworkset/elasticsearch/license/License.java
+++ src/main/java/org/frameworkset/elasticsearch/license/License.java
@@ -3,12 +3,16 @@ package org.frameworkset.elasticsearch.license;
 import org.frameworkset.elasticsearch.ElasticSearchHelper;
 import org.frameworkset.elasticsearch.client.ClientInterface;
 import org.frameworkset.elasticsearch.client.ClientUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class License {
+	private static Logger logger = LoggerFactory.getLogger(License.class);
 	public static void main(String[] args){
 		ClientInterface eventClientUtil = ElasticSearchHelper.getConfigRestClientUtil("conf/license.xml");
 		String ttt = eventClientUtil.executeHttp("_xpack/license?acknowledge=true","license", ClientUtil.HTTP_PUT);
-		System.out.println(ttt);
+//		System.out.println(ttt);
+		logger.info(ttt);
 		System.exit(1);
 	}
 }
